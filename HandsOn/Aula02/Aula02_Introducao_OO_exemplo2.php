9a4037f8e23b61ed37f5b20fc18bc67c8f6381cd
Sidcley1972 <sidcley_carvalho@hotmail.com>
2019-02-23 15:27:02 -0300

    "subindo acerto"

--- HandsOn/Aula02/Aula02_Introducao_OO_exemplo2.php
+++ HandsOn/Aula02/Aula02_Introducao_OO_exemplo2.php
@@ -1,5 +1,4 @@
 <?php
-
 class conta
 {
     public $saldo = 0;
@@ -9,7 +8,6 @@ class conta
      * metodo para fazer um saque
      * @param float $valor
      */
-
     public function sacar($valor)
     {
         echo "<hr>Saque Efetuado Valor: <b> $valor </b><hr>";
@@ -25,32 +23,47 @@ class conta
         echo "<hr>Depósito Efetuado Valor: <b> $valor </b><hr>";
         $this->saldo += $valor;
     }
-
     /**
      * retorna o saldo atual
      * *@return float $saldo
      */
-
     public function verSaldo()
     {
         return $this->saldo;
     } 
 }
-
 $conta1 = new Conta();
 $conta2 = new Conta();
 
 $conta1->titular = 'Luciana de Souza Lima';
-$conta1->titular = 'Aline de Oliveira';
+$conta2->titular = 'Aline de Oliveira';
 
 $conta1->depositar(500);
-$conta1->depositar(100);
+$conta2->depositar(1000);
 
-echo '<prep>';
+echo '<pre>';
 var_dump($conta1);
-echo '<br';
-var_dump($conta1);
+echo '<br>';
+var_dump($conta2);
+echo '<br>';
 
-echo '<hr';
-echo '<Saldo Atual em Conta1 ' .$conta1->verSaldo();
+echo '<hr>';
+echo 'Saldo Atual em Conta1 ' . $conta1->verSaldo();
+echo '<hr>';
+echo 'Saldo Atual em Conta2 ' . $conta2->verSaldo();
+echo '<hr>';
 
+$conta1->sacar(400);
+$conta2->sacar(600);
+
+echo '<hr>';
+echo 'Saldo Atual em Conta1 ' .$conta1->verSaldo();
+echo '<hr>';
+echo 'Saldo Atual em Conta2 ' .$conta2->verSaldo();
+echo '<hr>';
+
+echo '<hr>';
+echo 'Titular da Conta1 ' .$conta1->titular;
+echo '<hr>';
+echo 'Titular da Conta2 ' .$conta2->titular;
+echo '<hr>';
